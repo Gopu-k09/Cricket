4596e77fcc01d05f9601de6372430a5aa9840fb8
Gopu k <92145286+Gopuk@users.noreply.github.com>
2024-05-28 20:28:10 +0530

    Cricketers APP

--- Cricketers/views.py
+++ Cricketers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Cricketer
 # Create your views here.
+
+def home(request):
+    cricketers_list=Cricketer.objects.all()
+    data={
+        "cricketers":cricketers_list,
+    }
+    return render(request,"home.html",data)
+
